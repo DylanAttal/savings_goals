7280ef75d6a17c6902b0b9889a2c787520abadbc
Dylan Attal <attal.dylan@gmail.com>
2019-02-20 12:48:13 -0500

    Goal info passed down through props

--- client/src/Components/Goal/index.js
+++ client/src/Components/Goal/index.js
@@ -2,6 +2,15 @@ import React, { Component } from 'react'
 import './style.css'
 
 export default class Goal extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      goalName: this.props.goalName,
+      targetAmount: this.props.targetAmount,
+      savedAmount: this.props.savedAmount
+    }
+  }
   render() {
     return (
       <div className="goal">
@@ -10,16 +19,16 @@ export default class Goal extends Component {
             <p className="x">x</p>
           </div>
           <div className="name">
-            <h3>Car Down Payment</h3>
+            <h3>{this.state.goalName}</h3>
           </div>
         </div>
         <div className="savings">
           <div className="target">
-            <p>$4,000 Target</p>
+            <p>${this.state.targetAmount} Target</p>
             <button className="change-target">Change Target</button>
           </div>
           <div className="saved">
-            <p>$750 Saved</p>
+            <p>${this.state.savedAmount} Saved</p>
             <button className="add-savings">Add Savings</button>
           </div>
         </div>
--- client/src/Pages/Savings/index.js
+++ client/src/Pages/Savings/index.js
@@ -5,6 +5,10 @@ import './style.css'
 import Goal from '../../Components/Goal'
 
 export default class Savings extends Component {
+  constructor(props) {
+    super(props)
+  }
+
   render() {
     return (
       <div>
@@ -13,11 +17,27 @@ export default class Savings extends Component {
           <button className="logout">Log Out</button>
         </header>
         <section className="goals">
-          <Goal />
-          <Goal />
-          <Goal />
-          <Goal />
-          <Goal />
+          <Goal
+            goalName="Car Down Payment"
+            targetAmount={4000}
+            savedAmount={3000}
+          />
+          <Goal
+            goalName="House Down Payment"
+            targetAmount={25000}
+            savedAmount={10000}
+          />
+          <Goal
+            goalName="Bahamas Vacation"
+            targetAmount={3000}
+            savedAmount={500}
+          />
+          <Goal goalName="Roadtrip" targetAmount={1000} savedAmount={200} />
+          <Goal
+            goalName="Christmas Gifts"
+            targetAmount={500}
+            savedAmount={450}
+          />
         </section>
         <footer className="savings-footer">
           Thanks for saving at Acme Bank
