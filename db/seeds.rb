1f7375465e250ccb846bfc766c0a000d10890854
Dylan Attal <attal.dylan@gmail.com>
2019-02-21 13:50:00 -0500

    Adds dummy data to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(
+  first_name: 'Dylan',
+  last_name: 'Attal',
+  picture_url: 'https://i.kinja-img.com/gawker-media/image/upload/s--_DBGLHVf--/c_scale,f_auto,fl_progressive,q_80,w_800/eibgv7kctah62iddzywm.jpg'
+)
+
+Goal.create!(
+  name: 'Car Down Payment',
+  target: 4000,
+  saved: 3000,
+  user_id: 1
+)
+
+Goal.create!(
+  name: 'House Down Payment',
+  target: 25000,
+  saved: 10000,
+  user_id: 1
+)
+
+Goal.create!(
+  name: 'Bahamas Vacation',
+  target: 3000,
+  saved: 500,
+  user_id: 1
+)
+
+Goal.create!(
+  name: 'Roadtrip',
+  target: 1000,
+  saved: 200,
+  user_id: 1
+)
+
+Goal.create!(
+  name: 'Christmas Gifts',
+  target: 500,
+  saved: 450,
+  user_id: 1
+)
