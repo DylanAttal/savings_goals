5e886db16912bc03727fe0472239565497b970fb
Dylan Attal <attal.dylan@gmail.com>
2019-02-22 13:06:47 -0500

    Rails set up with Auth0

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
 class User < ApplicationRecord
   has_many :goals
+
+  def self.from_auth_hash(payload)
+    # Create a User for the first time.
+    User.find_or_create_by(auth_sub: payload["sub"]) do |user|
+      # Saves a user's first and last name
+      user.first_name = payload["given_name"]
+      user.last_name = payload["family_name"]
+      # Saves a user's picture as a URL
+      user.picture_url = payload["picture"]
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_20_181256) do
+ActiveRecord::Schema.define(version: 2019_02_22_180436) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2019_02_20_181256) do
     t.string "picture_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "auth_sub"
   end
 
   add_foreign_key "goals", "users"
